32d9d967fc8efb963f2383aecd4495e5b4d4958c
alexis <91147834+alexis-rudy@users.noreply.github.com>
2023-01-05 11:38:42 -0700

    finished bond program

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        //Console.WriteLine("Hello Prep1 World!");
+        //Opening title
+        Console.WriteLine("'Bond' Your Name!");
+        Console.WriteLine("---------------------------------");
+
+        //Get user's first name and save it in a variable.
+        Console.Write("What is your first name? ");
+        string fname = Console.ReadLine();
+        
+        //Get user's last name and save it in a variable.
+        Console.Write("What is your last name? ");
+        string lname = Console.ReadLine();
+
+        //Return the user's name in 'Bond' format.
+        Console.WriteLine();
+        Console.Write($"Your name is {lname}, {fname} {lname}.");
     }
 }
