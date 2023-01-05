9e29d37805f2400e1490bba46bf305bf3cd8b0c1
alexis <91147834+alexis-rudy@users.noreply.github.com>
2023-01-05 12:26:14 -0700

    finished grade program

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        //Console.WriteLine("Hello Prep2 World!");
+
+        int grade = 62;
+        string letter = "";
+
+        //Determine what the letter grade is based on the percentage.
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80 && grade < 90)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70 && grade < 80)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60 && grade < 70)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        //Write a message based on whether they passed.
+        if (grade > 70)
+        {
+            Console.WriteLine("Congrats on passing!");
+        }
+        else
+        {
+            Console.WriteLine("Don't worry, you can try again!");
+        }
+
+        //Print the letter grade to the user.
+        Console.Write($"Your grade is: {letter}.");
     }
 }
