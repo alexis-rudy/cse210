f40ea49dee97723a9565b7423c0b4e83a2d16240
alexis <91147834+alexis-rudy@users.noreply.github.com>
2023-01-05 12:07:26 -0700

    played with conditonals

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,23 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+
+        //Write an if statement
+        int x = 1;
+        int y = 3;
+        int e = 8;
+        if (x > y)
+        {
+            Console.WriteLine("x is bigger");
+        }
+        else if (e > x)
+        {
+            Console.WriteLine("e is bigger");
+            Console.BackgroundColor = ConsoleColor.Red;
+        }
+        else
+        {
+            Console.WriteLine("y is bigger");
+        }
     }
 }
