6d647eeb89bb2083752613899a94f7bff7797d4b
alexis <91147834+alexis-rudy@users.noreply.github.com>
2023-01-12 21:40:07 -0700

    completed base program

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        //Console.WriteLine("Hello Prep4 World!");
+
+        //Initialize all variables
+        List<int> numbers = new List<int>();
+        int userNumber;
+        int sum = 0;
+        int max = 0;
+        int count = 0;
+        int avg = 0;
+
+        //Get list items from user
+        do{
+            Console.Write("Enter a number: ");
+            string userItem = Console.ReadLine();
+            userNumber = int.Parse(userItem);
+            //Add userNumber to list
+            numbers.Add(userNumber);
+            sum += userNumber;
+            count++;
+            if (userNumber > max)
+            {
+                max = userNumber;
+            }
+        } while (userNumber != 0);
+        avg = sum/count;
+        Console.WriteLine($"Sum: {sum}");
+        Console.WriteLine($"Average: {avg}");
+        Console.WriteLine($"Largest Number: {max}");
     }
 }
