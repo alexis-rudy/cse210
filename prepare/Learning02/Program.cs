bb148dbe10e9c24d44d92ae482cc1dcd7725de56
alexis <91147834+alexis-rudy@users.noreply.github.com>
2023-01-20 03:20:10 -0700

    finished creating job class

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        //Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2022;
+        job1._endYear = 2025;
+        
+        job1.DisplayJob();
+
     }
 }
