1f045e2881058b7ec484ccad35c31416077076cb
alexis <91147834+alexis-rudy@users.noreply.github.com>
2023-01-20 09:26:27 -0700

    added resume class and display function

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -10,8 +10,20 @@ class Program
         job1._company = "Microsoft";
         job1._startYear = 2022;
         job1._endYear = 2025;
-        
         job1.DisplayJob();
+        
+        Job job2 = new Job();
+        job2._jobTitle = "Designer";
+        job2._company = "Vouge";
+        job2._startYear = 2023;
+        job2._endYear = 2025;
+        job2.DisplayJob();
+
+        Resume resume1 = new Resume();
+        resume1._name = "Jim Carter";
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+        resume1.Display();
 
     }
 }
