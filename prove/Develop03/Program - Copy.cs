6adb3f90fffbc3d5445c4fd24302dfaeeea37a5a
alexis <91147834+alexis-rudy@users.noreply.github.com>
2023-02-13 12:28:56 -0700

    added multi line scripture

--- prove/Develop03/Program - Copy.cs
+++ prove/Develop03/Program - Copy.cs
@@ -4,9 +4,22 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        string testText = "For behold this is my work and my glory to bring "
-            + "to pass the immortality and eternal life of  man.";
-        Scripture scripture = new Scripture(testText);
+        Scripture scripture;
+        Console.WriteLine("Do you want one verse or multiple?");
+        string doesUserWantMultipleVerses = Console.ReadLine();
+        if (doesUserWantMultipleVerses.ToLower() == "yes")
+        {
+            string reference = "John 14:15 If you love me, keep my commandments.";
+            scripture = new Scripture(reference);
+
+        }else
+        {
+            string reference = "Romans 13:8-10 8 Owe no man any thing, but to love one another: for he that loveth another hath fulfilled the law." +
+            " 9 For this, Thou shalt not commit adultery, Thou shalt not kill, Thou shalt not steal, Thou shalt not bear false witness, Thou shalt not covet; and if there" +
+            " any other commandment, it is briefly comprehended in this saying, namely, Thou shalt love thy neighbor as thyself. 10 Love worketh no ill to his neighbor: therefore love is the fulfilling of the law.";
+            scripture = new Scripture(reference);
+
+        }
 
         string choice = "";
         while(choice != "quit")
