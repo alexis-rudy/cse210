c55012de7c40bda1496fceabd92c83fbe9c10971
alexis <91147834+alexis-rudy@users.noreply.github.com>
2023-02-13 11:40:20 -0700

    added bro. manley's code

---
+++ prove/Develop03/Program - Copy.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string testText = "For behold this is my work and my glory to bring "
+            + "to pass the immortality and eternal life of  man.";
+        Scripture scripture = new Scripture(testText);
+
+        string choice = "";
+        while(choice != "quit")
+        {
+            
+            // 1. show scripture
+            string text = scripture.ToString();
+            Console.WriteLine(text);
+
+            // 2. wait for input
+            choice = Console.ReadLine();
+
+            // 3. if not quit hide a word
+            if (choice != "quit" || choice != "restart")
+            {
+                scripture.RandomlyHideWord();
+            }
+
+            // 4. restore whole thing
+            if (choice == "restart")
+            {
+                scripture.MakeAllWordsVisible();
+            }
+        }
+    }
+}
