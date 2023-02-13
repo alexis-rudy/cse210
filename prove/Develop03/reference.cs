90bd93e8dc75227f0f6408d5139a469451818ee5
alexis <91147834+alexis-rudy@users.noreply.github.com>
2023-02-13 12:28:29 -0700

    added a multi verse scripture

--- prove/Develop03/reference.cs
+++ prove/Develop03/reference.cs
@@ -3,4 +3,7 @@ using System;
 public class Reference
 {
     public string _reference = "John 14:15 If you love me, keep my commandments.";
+    public string _multipleScriptureReference = "Romans 13:8-10 8 Owe no man any thing, but to love one another: for he that loveth another hath fulfilled the law." +
+    " 9 For this, Thou shalt not commit adultery, Thou shalt not kill, Thou shalt not steal, Thou shalt not bear false witness, Thou shalt not covet; and if there" +
+    " any other commandment, it is briefly comprehended in this saying, namely, Thou shalt love thy neighbor as thyself. 10 Love worketh no ill to his neighbor: therefore love is the fulfilling of the law.";
 }
