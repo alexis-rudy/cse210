6f230dd8b2d3054966264e1c9119436830b23ac3
alexis <91147834+alexis-rudy@users.noreply.github.com>
2023-02-27 11:36:49 -0700

    added database class and complier files

---
+++ prove/journalProgram/Database.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+public class Database{
+    private Dictionary<string, string> database;
+
+    public void addToDictionary(string dateKey, string text)
+    {
+        database.Add(dateKey, text);
+    }
+    public string findItem(string date)
+    {
+        
+    }
+    
+}
