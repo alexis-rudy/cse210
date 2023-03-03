7488f3aad995d2cfa42edb544228d783c7e36da0
alexis <91147834+alexis-rudy@users.noreply.github.com>
2023-03-02 23:00:55 -0700

    started the class

---
+++ prove/MeditationProgram/ActivityLeader.cs
@@ -0,0 +1,12 @@
+// The resonsibility of an activity is to control the progress of meditation
+public class ActivityLeader{
+
+    public void WelcomeWithStartMessage(){
+
+    }
+
+    public void StopWithEndMessage(){
+
+    }
+
+}
---
+++ prove/MeditationProgram/BreathSpecialist.cs
@@ -0,0 +1,18 @@
+// Give instructions specific to the breathing activity
+
+public class BreathSpecialist{
+    string _breathin = "Breath in...";
+    string _breathout = "Breath out...";
+
+    public void GiveDescriptionOfBreathing(){
+    
+    }
+    public void TellGroupToBreathIn(){
+        Console.WriteLine(_breathin);
+    }
+    public void TellGroupToBreathOut(){
+        Console.WriteLine(_breathout);
+    }
+
+
+}
