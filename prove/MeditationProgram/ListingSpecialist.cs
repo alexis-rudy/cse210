3ee7e1114a462b549701951193afba683d8628b7
alexis <91147834+alexis-rudy@users.noreply.github.com>
2023-03-03 01:00:42 -0700

    completed basics for listing activity

--- prove/MeditationProgram/ListingSpecialist.cs
+++ prove/MeditationProgram/ListingSpecialist.cs
@@ -2,16 +2,24 @@ using System;
 using System.Collections.Generic;
 public class ListingSpecialist : ActivityLeader{
 
-    string _listingDescription;
-    List<string> _promptList;
+    string _listingDescription = "This activity will help you reflect on the good things in your life by having you list as many things as you can in a certain area.";
+    List<string> _promptList = [
+        "Who are people that you appreciate?",
+        "What are personal strengths of yours?",
+        "Who are people that you have helped this week?",
+        "When have you felt the Holy Ghost this month?",
+        "Who are some of your personal heroes?"
+    ];
     
     public void GiveListingDescription(){
-        
+        Console.WriteLine(_listingDescription);
     }
     public void GiveRandomQuestion(){
-
+        Console.WriteLine(_promptList[GetRandomInt()]);
     }
-    public void GetRandomPrompt(){
-
+    public int GetRandomInt(){
+        Random rand = new Random();
+        int randInt = rand.Next(0, _promptList.Count());
+        return randInt;
     }
 }
