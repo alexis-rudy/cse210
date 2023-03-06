10e3563e2960a1b76a5d27ab0d9959ccd65d01ea
alexis <91147834+alexis-rudy@users.noreply.github.com>
2023-03-06 15:05:26 -0700

    got to testing the getters and setters

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,44 @@
+using System;
+
+// The purpose of the Fraction class is to hold information about a fraction.
+public class Fraction{
+    private int _topNum;
+    private int _botNum;
+
+    //Constructors
+    public Fraction(){
+        _topNum = 1;
+        _botNum = 1;
+    }
+    public Fraction(int wholeNum){
+        _topNum = wholeNum;
+        _botNum = 1;
+    }
+    public Fraction(int top, int bottom){
+        _topNum = top;
+        _botNum = bottom;
+    }
+
+    // Getters and Setters 
+    public int GetTopNumber(){
+        return _topNum;
+    }
+    public int GetBotNumber(){
+        return _botNum;
+    }
+    public void SetTopNumber(int topNum){
+        _topNum = topNum;
+    }
+    public void SetBotNumber(int botNum){
+        _botNum = botNum;
+    }
+
+    // Methods
+    public string GetFractionView(int top, int bot){
+        string fraction = $"{top} \n - {bot}";
+        return fraction;
+    }
+    public double GetDecimalView(int top, int bot){
+        return top/bot;
+    }
+}
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        // Console.WriteLine("Hello Learning03 World!");
+
+        // No number
+        Fraction fraction = new Fraction();
+        // One number
+        Fraction fraction1 = new Fraction(4);
+        // Two numbers
+        Fraction fraction2 = new Fraction(3, 4);
+
+        // Trying getters and setters
+        Console.WriteLine(fraction.GetTopNumber());
+        Console.WriteLine(fraction1.GetTopNumber());
+        Console.WriteLine(fraction2.GetTopNumber());
+        Console.WriteLine(fraction1.SetTopNumber(5));
+        
+
     }
 }
