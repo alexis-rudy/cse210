a3597c2ab858ba31ebe9b7b6c1c1a173a40c2eb6
alexis <91147834+alexis-rudy@users.noreply.github.com>
2023-03-16 16:25:09 -0600

    fixed errors

--- prove/journalProgram/Program.cs
+++ prove/journalProgram/Program.cs
@@ -1,6 +1,13 @@
 using System;
 
 public class Program{
-    // Console.WriteLine("Please select one of the following choices: ");
-    // Console.W
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Please select one of the following choices: ");
+        Console.WriteLine("1. Write");
+        Console.WriteLine("2. Display");
+        Console.WriteLine("3. Load");
+        Console.WriteLine("4. Save");
+        Console.WriteLine("5. Quit");
+    }
 }
